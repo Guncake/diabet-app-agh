44d3e7107f41b79b134531cbabd4a843f9e501ac
Guncake <guncake001@gmail.com>
2021-06-02 12:27:55 +0200

    header addede

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import React,{Component} from 'react';
 import  Form from './components/Form.js'
 
 function App(){
-  return <Form />
+  return (
+  <div className="bkg">
+    <h1 className = "center">Cukrzycowa ankieta</h1>
+    <p className = "center">
+      Odpowiedz na pytania i zobacz se czy cukrzyce masz elo
+      <br />
+    </p>
+    <Form />
+  </div>
+  
+  )
 }
 export default App;
