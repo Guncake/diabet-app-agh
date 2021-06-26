d65f8e40017d6e17938ce7969a24fc25b6cda2bd
vorayo <kamil.hosaja@gmail.com>
2021-06-26 02:45:39 +0200

    myrdż

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = ({ show }) => {
     return (
         <div className={show ? 'sidenav.active' : 'sidenav'}>
             <h2 className='info'>Informacje o projekcie</h2>
-            <h3 className='info'>Projekt stworzył Kamil Górak powodznia w fetchowaniu</h3>
+            <h3 className='info'>Projekt stworzył Kamil Górak powodznia w fetchowaniiu</h3>
         </div>
     )
 }
